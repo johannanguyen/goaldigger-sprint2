592d382f1b3797299bc0fadd71dc4cf52b24ae9b
johannanguyen <johannatnguyen@gmail.com>
2020-11-07 10:22:13 +0000

    Combined Goals and Posts tables

--- models.py
+++ models.py
@@ -44,68 +44,67 @@ class Goals(db.Model):
         self.text = text
         
         
-"""
-class Posts(db.Model):
-    '''Table for user posts'''
-    id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String())
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    title = db.Column(db.String())
-    text = db.Column(db.String())
-    date = db.Column(db.DateTime, default=datetime.now)
+
+# class Posts(db.Model):
+#     '''Table for user posts'''
+#     id = db.Column(db.Integer, primary_key=True)
+#     category = db.Column(db.String())
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     title = db.Column(db.String())
+#     text = db.Column(db.String())
+#     date = db.Column(db.DateTime, default=datetime.now)
     
-    def __init__(self, user_id, category, title, text):
-        self.user_id = user_id
-        self.category = category
-        self.title = title
-        self.text = text
+#     def __init__(self, user_id, category, title, text):
+#         self.user_id = user_id
+#         self.category = category
+#         self.title = title
+#         self.text = text
 
-class Messages(db.Model):
-    '''Table for chat messages.'''
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(300))
-    date = db.Column(db.DateTime, default=datetime.now)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+# class Messages(db.Model):
+#     '''Table for chat messages.'''
+#     id = db.Column(db.Integer, primary_key=True)
+#     text = db.Column(db.String(300))
+#     date = db.Column(db.DateTime, default=datetime.now)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
-    def __init__(self, text, user_id):
-        self.text = text
-        self.user_id = user_id
+#     def __init__(self, text, user_id):
+#         self.text = text
+#         self.user_id = user_id
 
-    def __repr__(self):
-        return "<Text: {}\nBy: {}>\n".format(self.text, self.user)
+#     def __repr__(self):
+#         return "<Text: {}\nBy: {}>\n".format(self.text, self.user)
         
         
-class Groups(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    category = db.Column(db.String())
-    description = db.Column(db.String())
-    name = db.Column(db.String())
-    sidebar_text = db.Column(db.String())
-    created = db.Column(db.DateTime, default=datetime.now)
+# class Groups(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     category = db.Column(db.String())
+#     description = db.Column(db.String())
+#     name = db.Column(db.String())
+#     sidebar_text = db.Column(db.String())
+#     created = db.Column(db.DateTime, default=datetime.now)
     
-    def __init__(self, category, description, name, sidebar_text):
-        self.category = category
-        self.description = description
-        self.name = name
-        self.sidebar_text = sidebar_text
+#     def __init__(self, category, description, name, sidebar_text):
+#         self.category = category
+#         self.description = description
+#         self.name = name
+#         self.sidebar_text = sidebar_text
         
-class Groups_Users(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
+# class Groups_Users(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+#     group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
     
-    def __init__(self, user_id, group_id):
-        self.user_id = user_id
-        self.group_id = group_id
+#     def __init__(self, user_id, group_id):
+#         self.user_id = user_id
+#         self.group_id = group_id
 
 
-Maybe this Enum is unnecesary.
-class Progress(Enum):
-    IN_PROGRESS = "in progress"
-    FINISHED = "completed"
+# Maybe this Enum is unnecesary.
+# class Progress(Enum):
+#     IN_PROGRESS = "in progress"
+#     FINISHED = "completed"
 
-class Categories(Enum):
-    WORK = "work"
-    EXERCISE = "exercise"
-    ...
-'''
+# class Categories(Enum):
+#     WORK = "work"
+#     EXERCISE = "exercise"
+#     ...
