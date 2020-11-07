f9a182965fa8e13d8bc715c5db3f3b978024b1c7
EC2 Default User <ec2-user@ip-172-31-49-111.ec2.internal>
2020-11-07 00:09:11 +0000

    Added code to app.py to start database object. Removed static/script.js from gitignore. Updated script.js changes

--- app.py
+++ app.py
@@ -3,12 +3,26 @@ import flask
 from flask import Flask, render_template
 import flask_socketio
 import flask_sqlalchemy
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 
 
 app = Flask(__name__)
 server_socket = flask_socketio.SocketIO(app)
 server_socket.init_app(app, cors_allowed_origins="*")
 
+dotenv_path = join(dirname(__file__), "sql.env")
+load_dotenv(dotenv_path)
+database_uri = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = database_uri
+
+db = flask_sqlalchemy.SQLAlchemy(app)
+db.init_app(app)
+db.app = app
+
+db.create_all()
+db.session.commit()
 
 @app.route("/", methods=["GET", "POST"])
 def hello():
