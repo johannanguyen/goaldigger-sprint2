188597fc70f8e692a838366bc018630b178ba5f4
EC2 Default User <ec2-user@ip-172-31-49-111.ec2.internal>
2020-11-12 19:42:57 +0000

    Changed the way goals are sent so that the client receives one array instead of two

--- pages/UserProfile.jsx
+++ pages/UserProfile.jsx
@@ -2,64 +2,60 @@ import * as React from 'react';
 import { clientSocket } from '../scripts/Socket';
 
 export default function UserProfile() {
-    const [users, setUsers] = React.useState([]);
+    const [user, setUser] = React.useState([]);
     const [goals, setGoals] = React.useState([]);
     const [progress, setProgress] = React.useState([]);
 
     function getGoogleUserInfo() {
-    React.useEffect(() => {
-      clientSocket.on('google info received', (data) => {
-        console.log("Received this google info from server: ", data);
-        setUsers(data);
-      });
-    });
-  }
+      React.useEffect(() => {
+        clientSocket.on("google info received", updateGoogleUserInfo)
+        return () => {
+          clientSocket.off("google info received", updateGoogleUserInfo)
+        }
+      })
+    }
+    
+    function updateGoogleUserInfo(data) {
+      setUser(data)
+    }
   
     function getGoalInfo() {
-    React.useEffect(() => {
-      clientSocket.on('goal_description', (data) => {
-        console.log("Received goal info: ", data);
-        setGoals(data);
-      });
-    });
-  }
-  
-      function getProgressInfo() {
-    React.useEffect(() => {
-      clientSocket.on('goal_progress', (data) => {
-        console.log("Received goal info: ", data);
-        setProgress(data);
-      });
-    });
-  }
-  
-  
+      React.useEffect(() => {
+        clientSocket.on('user goals', updateGoalInfo)
+        return () => {
+          clientSocket.off('user goals', updateGoalInfo)
+        }
+      })
+    }
+      
+    function updateGoalInfo(data) {
+      console.log("Received goal info: ", data)
+      setGoals(data)
+    }
+
   getGoogleUserInfo();
   getGoalInfo();
-  getProgressInfo();
   
-    return(
-        <div className="root_container">
-          <div className="content_container">
-              <h1>{users["username"]}</h1>
-               <br />
-              
-              <img src={users["image"]}></img>
-              <br />
-              <br />
-              
-              <h3>Here's a list of my goals:</h3>
-
-              <div className="goal_container">
-                 { progress.map((data, index) => (
-                  <div>
-                      <b>{data}:</b> {goals[index]}
-                      <br />
-                  </div>)) }
-          
-              
-              </div>
+  return(
+    <div className="root_container">
+      <div className="content_container">
+        <h1>{user["username"]}</h1>
+        <br />
+        
+        <img src={user["image"]}></img>
+        <br />
+        <br />
+        
+        <h3>Here's a list of my goals:</h3>
+        <div className="goal_container">
+        { goals.map((data, index) =>
+          <div>
+            <b>{data.progress}:</b> {data.description}
+            <br />
           </div>
+        )}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- scripts/GoogleLogin.jsx
+++ scripts/GoogleLogin.jsx
@@ -8,30 +8,20 @@ const responseGoogle = (response) => {
 }
 
 function get_info(google_user) {
-    var id_token = google_user.getAuthResponse().id_token;
-    let email = google_user.profileObj.email;
-    let username = google_user.profileObj.name;
-    let image = google_user.profileObj.imageUrl;
-    let is_signed_in = google_user.isSignedIn();
-    
-    //console.log('Sent the following google user info to server!' + id_token + email + username + image + is_signed_in);
-    
-    
-    clientSocket.emit('new google user', {
-        'id_token': id_token,
-        'email': email,
-        'username': username,
-        'image': image,
-        'is_signed_in': is_signed_in
-    });
 
+    clientSocket.emit('new google user', {
+        'id_token': google_user.profileObj.googleId,
+        'email': google_user.profileObj.email,
+        'username': google_user.profileObj.name,
+        'image': google_user.profileObj.imageUrl,
+    });
 }
 
 export function GoogleButton() {
     return (
         <div>
             <GoogleLogin
-            clientId="791115456005-sqrq5ha01c9bmcbe7c7u6lco4p9l4r1b.apps.googleusercontent.com"
+            clientId="1062054290390-k78ra3cikp1topp72a1s8bo02m965adi.apps.googleusercontent.com"
             buttonText="Login"
             onSuccess={get_info}
             onFailure={responseGoogle}
